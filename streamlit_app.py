f5e6e9d58c84305b26de80005d2d9b485499c793
dchapmanfw <chapdani@gmail.com>
2024-09-05 16:45:03 +0000

    basic skeleton

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def main():
+    st.title("Brewmaster's Bin")
+    
+    name = st.text_input("Enter your brew:")
+    description = st.text_area("Describe your brew:")
+    
+    if st.button("Submit"):
+        st.write("Name:", name)
+        st.write("Description:", description)
+
+    st.write("##")
+    
+    if st.button("PICK YOUR POISON"):
+        pass
+
+main()
